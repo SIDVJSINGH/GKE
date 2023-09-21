692bbec92332e8df95399e28658059fbe50bb356
SIDVJSINGH <77890467+SIDVJSINGH@users.noreply.github.com>
2023-09-21 22:47:34 +0530

    Add files via upload

---
+++ 1provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+  backend "gcs" {
+    bucket         = "terrateam-demo-109032"
+    prefix         = "GKS/gks.tfstate"
+  }
+}
+
+
+# to set region in the gcp, go to shell and type this command "gcloud config set compute/region us-cental1-a"
+# to set zone in the gcp, go to shell and type this command "gcloud config set compute/zone us-cental1"
+
+provider "google" {
+  project     = "terrateam-demo"
+  region      = "us-central1"
+  zone        = "us-central1-a"
+}
+
---
+++ 2vpc.tf
@@ -0,0 +1,32 @@
+resource "google_project_service" "compute" {
+  project = "terrateam-demo" 
+  service = "compute.googleapis.com"
+  disable_on_destroy = true
+  disable_dependent_services = true
+}
+
+resource "google_project_service" "cloudresourcemanager" { 
+  project = "terrateam-demo" 
+  service = "cloudresourcemanager.googleapis.com"
+  disable_on_destroy = true
+  disable_dependent_services = true
+}
+
+resource "google_project_service" "container" {
+  project = "terrateam-demo" 
+  service = "container.googleapis.com"
+  disable_on_destroy = true
+  disable_dependent_services = true
+}
+
+data "google_client_config" "default" {}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = lower("network")
+  auto_create_subnetworks = false
+  routing_mode = "REGIONAL"
+  mtu = 1460
+  delete_default_routes_on_create = false
+
+  depends_on = [ data.google_client_config.default, google_project_service.compute, google_project_service.container ]
+}
